1f40275f3e93a6f57c97c81114cff2caf98a160c
Anton Klochkov <tklochkov@gmail.com>
2018-03-25 23:20:32 +0300

    Added scipts for create and drop database

---
+++ DB-scripts/DropBase.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS "vdv_court";
+
+DROP TABLE IF EXISTS "vdv_user";
+
+DROP TABLE IF EXISTS "vdv_media";
+
+DROP TABLE IF EXISTS "vdv_court_follower";
+
+DROP TABLE IF EXISTS "vdv_user_follower";
+
+DROP TABLE IF EXISTS "vdv_location";
+
+DROP TABLE IF EXISTS "vdv_court_prop";
+
+DROP TABLE IF EXISTS "vdv_court_prop_bool";
+
+DROP TABLE IF EXISTS "vdv_court_prop_numeric";
+
+DROP TABLE IF EXISTS "vdv_court_prop_media";
+
+DROP TABLE IF EXISTS "vdv_court_prop_comment";
+
+DROP TABLE IF EXISTS "vdv_post";
+
+DROP TABLE IF EXISTS "vdv_comment";
+
+DROP TABLE IF EXISTS "vdv_post_prop";
+
+DROP TABLE IF EXISTS "vdv_like";
+
+DROP TABLE IF EXISTS "vdv_court_prop_like";
+
+DROP TABLE IF EXISTS "vdv_post_like";
+
+DROP TABLE IF EXISTS "vdv_post_media";
+
+DROP TABLE IF EXISTS "vdv_post_comment";
