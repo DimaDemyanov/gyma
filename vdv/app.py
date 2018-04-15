d5342c7ad80fe883ca983ea9dde0a6ebc855bfec
daniilsavchuk <dsavchuk@itsociety.su>
2018-04-15 03:39:22 +0300

    Mirrored auth

--- vdv/app.py
+++ vdv/app.py
@@ -785,7 +785,7 @@ with open(cfgPath) as f:
 
 general_executor = ftr.ThreadPoolExecutor(max_workers=20)
 
-wsgi_app = api = falcon.API(middleware=[CORS(), Auth(), MultipartMiddleware()])
+wsgi_app = api = falcon.API(middleware=[CORS(), MultipartMiddleware()])#Auth(),
 
 server = SpecServer(operation_handlers=operation_handlers)
 
