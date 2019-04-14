cd8eb82e9e392e29db1feb8ee0cf47c4b4e6049d
zzaakiirr <pfrbhxbr@gmail.com>
2019-04-14 14:59:01 +0300

    Fixed invalid test in GetUserByPhoneTests

--- vdv/Tests/Account/GetUserByPhoneTests.py
+++ vdv/Tests/Account/GetUserByPhoneTests.py
@@ -15,9 +15,16 @@ class GetMyUserTests(BaseTestCase):
         self.check_operation_id_has_operation_handler(operation_id)
         self.request_uri_path = self.get_request_uri_path(operation_id)
 
-        self.valid_request_params = {"params": {"phone": TEST_ACCOUNT['phone']}}
-        self.invalid_request_params = {"params": {"phone": "invalid param"}}
-        self.non_existing_phone_request_params = {"params": {"phone": "0"}}
+        self.valid_request_params = {
+            "params": {
+                "phone": TEST_ACCOUNT['phone']
+            }
+        }
+        self.non_existing_phone_request_params = {
+            "params": {
+                "phone": "0"
+            }
+        }
 
     # Tests
 
@@ -31,14 +38,12 @@ class GetMyUserTests(BaseTestCase):
         self.assertEqual(resp.status, falcon.HTTP_200)
         self.check_dict1_in_dict2(resp.json, TEST_ACCOUNT)
 
-    def test_get_user_given_invalid_phone_param(self):
+    def test_get_user_not_given_phone_param(self):
         # When
-        resp = self.client.simulate_get(
-            self.request_uri_path, **self.invalid_request_params
-        )
+        resp = self.client.simulate_get(self.request_uri_path)
 
         # Then
-        self.assertEqual(resp.status, falcon.HTTP_405)
+        self.assertEqual(resp.status, falcon.HTTP_407)
 
     def test_get_user_given_non_existing_phone_param(self):
         # When
